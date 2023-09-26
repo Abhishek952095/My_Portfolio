8c6a0ea0ba811ac224c21987ea8420e609b146dd
Abhishek Sharma <146001382+Abhishek952095@users.noreply.github.com>
2023-09-26 13:42:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let popup = document.getElementById("popup");
+
+function openpopup(){
+    popup.classList.add("openpopup")
+}
+function closepopup(){
+    popup.classList.remove("openpopup")
+}
+
+let slide = document.getElementById("slide");
+
+function openpopup(){
+   slide.classList.add("openpopup")
+}
